61026fa1aed79caa1cf401ca7c400a3b4ae3de66
The Judge <colepatirckroberts@gmail.com>
2024-04-20 13:37:22 -0700

    Vivecraft OpenXR

--- src/main/jni/Android.mk
+++ src/main/jni/Android.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := $(call my-dir)
+ LOCAL_PATH := $(call my-dir)
 HERE_PATH := $(LOCAL_PATH)
 
 # include $(HERE_PATH)/crash_dump/libbase/Android.mk
@@ -30,35 +30,9 @@ LOCAL_SRC_FILES := tinywrapper/main.c tinywrapper/string_utils.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/tinywrapper
 include $(BUILD_SHARED_LIBRARY)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := openxr_loader
-LOCAL_SRC_FILES := libopenxr_loader.so
-include $(PREBUILT_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := occore
-LOCAL_SRC_FILES := ./OpenOVR/libOCCore.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := openxr_loader
-LOCAL_MODULE := drvopenxr
-LOCAL_SRC_FILES := ./OpenOVR/libDrvOpenXR.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := ocovr
-LOCAL_STATIC_LIBRARIES := drvopenxr occore drvopenxr
-LOCAL_SRC_FILES := ./OpenOVR/OCOVR.a
-include $(PREBUILT_STATIC_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_LDLIBS := -llog -landroid -lGLESv3 -lvulkan -lEGL
-LOCAL_CFLAGS := -DXR_USE_PLATFORM_ANDROID -DXR_USE_GRAPHICS_API_OPENGL_ES
-LOCAL_SHARED_LIBRARIES := openxr_loader
-LOCAL_WHOLE_STATIC_LIBRARIES := ocovr
-LOCAL_MODULE := openvr_api
+LOCAL_MODULE := vloader
         LOCAL_SRC_FILES := \
                     vloader.cpp
 include $(BUILD_SHARED_LIBRARY)
@@ -69,7 +43,7 @@ LOCAL_LDLIBS := -ldl -llog -landroid -lGLESv3 -lEGL
 # -lGLESv2
 LOCAL_MODULE := pojavexec
 # LOCAL_CFLAGS += -DDEBUG
-LOCAL_SHARED_LIBRARIES := openvr_api regal
+LOCAL_SHARED_LIBRARIES := regal
 # -DGLES_TEST
 LOCAL_SRC_FILES := \
     egl_bridge.c \
